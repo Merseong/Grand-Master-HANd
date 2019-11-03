8183b6da7a31145a07db3c95f689cb5f56932cf5
JeongWooSeok <www013181@gmail.com>
2019-11-03 13:59:08 +0900

    boss 기본 공격 틀 구현

--- Escape Breath!/Assets/Scripts/Boss.cs
+++ Escape Breath!/Assets/Scripts/Boss.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Boss : MonoBehaviour
 {
     public int health;
+    public int phase;
+    public int attackType;
     public Transform bossTarget;
     public TextMesh testTextMesh;
+    public Object attackArea;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -34,4 +38,83 @@ public class Boss : MonoBehaviour
 
         }
     }
+    public void readyAttack(int phase) //공격 준비 단계
+    {
+        int ran = Random.Range(0, 2);
+        bool isClose = false;
+        for (int b = 5; b < 8; b++)
+        {
+            for(int a = 0; a < 8; a++)
+            {
+                if (GameManager.inst.chessBoard.GetPiece(a, b) == null)
+                    isClose = true;
+            }
+        }
+        if (isClose == true)
+        {
+
+        }
+        else
+        {
+            switch (phase)
+            {
+                case 1:
+                    if (ran == 0)
+                    {
+                        meteor(1);
+                        attackType = 10;
+                    }
+                    else if (ran == 1)
+                    {
+                        lazer(1);
+                        attackType = 11;
+                    }
+                    else if (ran == 2)
+                    {
+                        attackType = 12;
+                    }
+                    break;
+            }
+        }
+    }
+    public void attack(int attackType) //ㄹㅇ 공격
+    {
+        switch (attackType){
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+        }
+    }
+
+    public void meteor(int phase)
+    {
+        int x, y;
+        var Board = GameManager.inst.chessBoard;
+
+        if(phase == 0)
+        {
+            for(int i =0; i <4; i++)
+            {
+                x = (int)Random.Range(1, 8);
+                y = (int)Random.Range(1, 4);
+                Instantiate(attackArea, Board.IndexToGlobalPos(x, y), Quaternion.identity); //제대로 생성되는지 확인 요망
+            }
+        }else
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                x = (int)Random.Range(1, 8);
+                y = (int)Random.Range(1, 4);
+                Instantiate(attackArea, Board.IndexToGlobalPos(x, y), Quaternion.identity); //제대로 생성되는지 확인 요망
+            }
+        }
+    }
+    public void lazer(int phase)
+    {
+
+    }
+    public 
 }
