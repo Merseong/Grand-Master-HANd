124430589e7df05612eb0e500705e15bca87cc49
Merseong <esc990720@korea.ac.kr>
2019-11-15 00:15:44 +0900

    done init scene to explain

---
+++ Escape Breath!/Assets/Scripts/BeforeStartScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BeforeStartScript : MonoBehaviour
+{
+    public Transform kingTr;
+    public Transform bossTr;
+
+    private bool isStarting = false;
+    private Vector3 bossStartPos;
+    private Vector3 bossFinalPos;
+
+    private void Start()
+    {
+        bossFinalPos = bossTr.position;
+        bossStartPos = bossFinalPos + new Vector3(0, 10, 0);
+        bossTr.position = bossStartPos;
+        bossTr.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == kingTr && !isStarting)
+        {
+            isStarting = true;
+            StartCoroutine(LoadSceneAsyncCoroutine());
+        }
+    }
+
+    IEnumerator LoadSceneAsyncCoroutine()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(1);
+        asyncLoad.allowSceneActivation = false;
+        bossTr.gameObject.SetActive(true);
+
+        float timer = 0;
+        while (!asyncLoad.isDone && timer < 1f)
+        {
+            timer += Time.deltaTime;
+            bossTr.position = Vector3.Lerp(bossStartPos, bossFinalPos, timer);
+            yield return null;
+        }
+
+        asyncLoad.allowSceneActivation = true;
+    }
+}
